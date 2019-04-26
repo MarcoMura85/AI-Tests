c21dc3fd19094999d51635cbca0e239547b8072f
MarcoMura85 <marcomura85@gmail.com>
2019-04-26 21:33:37 +0200

    fix exit game

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 import carClass
 
-
 # initialize the pygame module
 pygame.init()
 # load and set the logo
@@ -18,14 +17,8 @@ car = carClass.Car(100,100,"car.png", screen)
 car.showCar(screen)
 
 def game_loop():
-    # define the position of the smily
     xpos = screen_width/2
     ypos = screen_height/2
-    # how many pixels we move our smily each frame
-    step_x = 10
-    step_y = 10
-
-    # and blit it on scree
     car.setPos(xpos, ypos, 0)
 
     # update the screen to make the changes visible (fullscreen update)
@@ -36,64 +29,16 @@ def game_loop():
 
     # main loop
     while running:
-
-        delta_x = 0
-
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                #pygame.quit()
                 running = False
 
-        #     if event.type == pygame.KEYDOWN:
-        #         if event.key == pygame.K_LEFT:
-        #             left = True
-        #         if event.key == pygame.K_RIGHT:
-        #             right = True
-        #         if event.key == pygame.K_UP:
-        #             up = True
-        #         if event.key == pygame.K_DOWN:
-        #             down = True
-        #
-        #     if event.type == pygame.KEYUP:
-        #         if event.key == pygame.K_LEFT:
-        #             left = False
-        #         if event.key == pygame.K_RIGHT:
-        #             right = False
-        #         if event.key == pygame.K_UP:
-        #             up = False
-        #         if event.key == pygame.K_DOWN:
-        #             down = False
-        #
-        # # update the position of the smily
-        # if left: xpos -= step_x
-        # if right: xpos += step_x
-        # if up: ypos -= step_y
-        # if down: ypos += step_y
-        #
-        # if xpos > screen_width - car_img_size[0]:
-        #     right = False
-        #     xpos = screen_width - car_img_size[0]
-        # if xpos < 0:
-        #     left = False
-        #     xpos = 0
-        #
-        # if ypos > screen_height - car_img_size[1]:
-        #     down = False
-        #     ypos = screen_height - car_img_size[1]
-        # if ypos < 0:
-        #     up = False
-        #     ypos = 0
-
-
         screen.fill((128, 128, 128))
-        # now blit the smily on screen
-        #displayCar(xpos, ypos)
         car.updatePosition(event)
         car.showCar(screen)
 
-        # and update the screen (dont forget that!)
         pygame.display.update()
         clock.tick(60)
 
