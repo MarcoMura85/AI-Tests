24c68da78a8ba04871d7571e3a29411767dbb559
MarcoMura85 <marcomura85@gmail.com>
2019-04-27 22:01:00 +0200

    creating a personal rectangle class for collision detection

--- rectClass.py
+++ rectClass.py
@@ -1,4 +1,6 @@
 import pygame
+import numpy
+import math
 
 class myRect(pygame.Surface):
     def __init__(self, surface, rect, angle):
@@ -26,11 +28,25 @@ class myRect(pygame.Surface):
     def updatePos(self, xpos, ypos, angle):
         self.xpos = xpos - self.width/2
         self.ypos = ypos - self.height/2
+        self.angle = angle
+
+        #angle = 65.
+        theta = (self.angle)
+
+        rotMatrix = numpy.array([[numpy.cos(theta), -numpy.sin(theta)],
+                                 [numpy.sin(theta), numpy.cos(theta)]])
+
+
         self.P0 = (self.xpos, self.ypos)
         self.P1 = (self.xpos, self.ypos + self.height)
         self.P2 = (self.xpos + self.width, self.ypos + self.height)
         self.P3 = (self.xpos + self.width, self.ypos)
-        self.angle = angle
+
+#        self.P0 = numpy.dot(rotMatrix, self.P0)
+
+        qx = xpos + math.cos(theta) * (self.P0[0] - xpos) - math.sin(theta) * (self.P0[1] - ypos)
+        qy = ypos + math.sin(theta) * (self.P0[0] - xpos) + math.cos(theta) * (self.P0[1] - ypos)
+        self.P0 = (qx, qy)
 
     def drawMyRect(self):
         pygame.draw.line(self.surface, (0, 0, 255), self.P0, self.P1)
